b6148859ec9efa4fbe3ef3ede3e5f552708b91f8
dainbrownlow08 <dain.brownlow@outlook.com>
2021-01-17 22:42:47 -0800

    commit tree 3365c6e40a2bb7d169e68a6aab8b8e22337037aa

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solution here
+function findMatching(drivers,str){
+    let a = drivers.filter(n => {
+        return  n === str || str.toLowerCase() === n
+    })
+    return a
+}
+
+function fuzzyMatch(drivers,str){
+    let a = drivers.filter(n => {
+        return str.substring(0,2) === n.substring(0,2)
+    })
+    return a
+}
+
+function matchName(drivers,str){
+    let a = drivers.filter(n => {
+        return n.name === str
+    })
+    return a
+}
